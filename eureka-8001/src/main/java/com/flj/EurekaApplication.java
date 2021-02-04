de0fc87294771f9fc6a658870608be027d76b2bd
2017LLLLL <760352276@qq.com>
2021-02-04 11:38:37 +0800

    增加eureka

---
+++ eureka-8001/src/main/java/com/flj/EurekaApplication.java
@@ -0,0 +1,12 @@
+package com.flj;
+
+/**
+ * @PackageName com.flj
+ * @Classname EurekaApplication
+ * @Description TODO
+ * @Date 2021/2/4 11:38
+ * @Author flj
+ * @Version 1.0.0
+ */
+public class EurekaApplication {
+}
